f9da532ebd13515f97ff43280b687246f50d08f8
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-29 03:14:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let square=(number)=>number**2
+
+/*
+
+8kyu- Function 2 - squaring an argument
+
+Now you have to write a function that takes an argument and returns the square of it.
+
+
+https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
